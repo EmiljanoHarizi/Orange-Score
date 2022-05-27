bdc6b48f241c969ae811ffe1f84f2e0cf247eff2
Emiljano Harizi <emilio.harizi@outlook.com>
2022-05-27 13:30:58 +0300

    Removed R4 form and layout for adding players

--- app/src/main/java/com/example/orangescore/R4.java
+++ app/src/main/java/com/example/orangescore/R4.java
@@ -16,12 +16,8 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class R4 extends AppCompatActivity {
+    Button logout_button;
 
-    EditText team, name, position, photo;
-    String team_str, name_str, position_str, photo_str;
-    Button button_submit, logout_button;
-
-    private FirebaseFirestore DB = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +25,6 @@ public class R4 extends AppCompatActivity {
         setContentView(R.layout.activity_r4);
 
         logout_button = findViewById(R.id.logout_btn);
-        button_submit = findViewById(R.id.submit_btn);
 
         logout_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,29 +34,7 @@ public class R4 extends AppCompatActivity {
                 finish();
             }
         });
-    }
-
-    public void onSubmitClick(View view) {
-        team = findViewById(R.id.teamEditText);
-        name = findViewById(R.id.nameEditText);
-        position = findViewById(R.id.positionEditText);
-        photo = findViewById(R.id.photoEditText);
-
-        team_str = team.getText().toString();
-        name_str = name.getText().toString();
-        position_str = position.getText().toString();
-        photo_str = photo.getText().toString();
-
-        Player p = new Player(team_str, name_str, position_str, photo_str);
-        Log.d("R4", "Submitted player" + p.getPlayer_name()  + "from team" + p.getPlayer_team());
-
-        DB.collection("players").add(p).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d("R4", "Player entry added successfully");
-                Toast.makeText(R4.this, "Player has been added ", Toast.LENGTH_SHORT).show();
-            }
-        });
 
     }
+
 }

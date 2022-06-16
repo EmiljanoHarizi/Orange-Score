876eb24af2bb0689df0b1f72ef07a744d3a4486a
MariaDrin <mariadrin1717@gmail.com>
2022-06-16 23:03:15 +0300

    i added button to r8  and it works

--- app/src/main/java/com/example/orangescore/R8_Activity.java
+++ app/src/main/java/com/example/orangescore/R8_Activity.java
@@ -2,13 +2,42 @@ package com.example.orangescore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class R8_Activity extends AppCompatActivity {
 
+    Button logout_button;
+    Button r8tor9_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_r8);
+
+        logout_button = findViewById(R.id.logout_btn);
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+
+        r8tor9_btn = findViewById(R.id.r8tor9_btn);
+
+        r8tor9_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), R9_Activity.class));
+                finish();
+            }
+        });
     }
 }
